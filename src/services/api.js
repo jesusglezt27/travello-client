35e06b7e72e8ca53b08d073bf7008367e61b57a2
Jesus Gonzalez <jesusglezt27@gmail.com>
2023-10-15 17:44:10 -0600

    vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const _api = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:5005/api",
+    baseURL: process.env.REACT_APP_SERVER_URL || "https://travello-server-rose.vercel.app/api",
     timeout:10000
 })
 
